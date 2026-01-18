4620666be7d5dfd12cfe005d6105e37617e43d93
Linh Nguyen <linhnguyen0132@gmail.com>
2026-01-18 04:20:18 -0500

    add scope gmail.readonly to oauth.py

--- backend/routes/oauth.py
+++ backend/routes/oauth.py
@@ -26,7 +26,7 @@ SCOPES = [
     'openid',
     'https://www.googleapis.com/auth/userinfo.email',
     'https://www.googleapis.com/auth/userinfo.profile',
-    #'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/gmail.readonly',
     #'https://www.googleapis.com/auth/calendar.events'
 ]
 
